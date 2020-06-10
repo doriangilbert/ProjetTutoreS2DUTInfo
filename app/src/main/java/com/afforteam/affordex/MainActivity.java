c1e72b92f1e0b3d3cee8445ab6e590ec0f200c9a
Hirotrex <r.morel@live.fr>
2020-06-10 22:38:22 +0200

    Implémentation de la génération de l'affichage des pokémons et communication avec la BD

--- app/src/main/java/com/afforteam/affordex/MainActivity.java
+++ app/src/main/java/com/afforteam/affordex/MainActivity.java
@@ -9,9 +9,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.google.android.material.navigation.NavigationView;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer; //App drawer (= Les 3 traits en haut à droite de l'écran) permettant d'accéder au menu latéral en cliquant dessus
@@ -38,6 +49,67 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentAffordex()).commit();
             navigationView.setCheckedItem(R.id.nav_affordex);
         }
+
+        setContentView(R.layout.activity_main);
+
+        //Récupération des informations de la BD
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://78.204.206.61:3306/ProjetTuto_DB?autoReconnect=true&useSSL=false", "Richard", "Raymond");
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select * from POKEMON");
+
+            int i = 0;
+            // Tant qu'il y a des résultats
+            while (rs.next()) {
+
+                i++;
+
+                setContentView(R.layout.activity_main);
+
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.LinearLayout);
+                //Création d'une "Tuile" pour chaque pokémon
+                RelativeLayout pokemon = new RelativeLayout(this);
+                linearLayout.addView(pokemon);
+
+                pokemon.getLayoutParams().width = RelativeLayout.LayoutParams.MATCH_PARENT;
+                pokemon.getLayoutParams().height = 70;
+                pokemon.setId(i);
+                //Création des différents attributs de chaque pokémon
+                TextView nomP = new TextView(this);
+                nomP.setText(rs.getString(2));// Réxupération de la colonne 2 qui contient les noms de pokémon
+                pokemon.addView(nomP);
+
+                ImageView imageP = new ImageView(this);
+                //imageP.setImageResource("cheminImage" + rs.getString(2)); A implémenter avec le stockage des images dans la mémoire du téléphone
+                pokemon.addView(imageP);
+
+                TextView typeP = new TextView(this);
+                typeP.setText(rs.getString(4));
+                pokemon.addView(typeP);
+
+                TextView numP = new TextView(this);
+                numP.setText(rs.getString(1));
+                pokemon.addView(numP);
+
+                TextView genP = new TextView(this);
+                genP.setText(rs.getString(13));
+                pokemon.addView(genP);
+            }
+
+            con.close();
+        }
+
+        catch(Exception e){
+
+            e.printStackTrace();
+        }
+
     }
 
     //Cette procédure permet d'éviter de quitter l'application lorsque le menu latéral est ouvert
